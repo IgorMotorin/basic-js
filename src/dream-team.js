eb70256624d70cf172aaf6c2ff1c3440c60cec1f
Igor Motorin <igor.motorin@gmail.com>
2020-10-01 17:47:49 +0300

    feat: add function dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
+module.exports = function createDreamTeam(members) {
+
+  if (Array.isArray(members) !== true) {
+    return false;
+  }
+let arrFilter = members.filter(a => typeof a === "string");
+let arrMap = arrFilter.map(function (str) {return str.replace(/\s/g, '')[0];})
+let arrSort = Array.from(arrMap.sort().join("").toUpperCase());
+arrSort = arrSort.sort().join("");
+
+return arrSort;
+  
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
