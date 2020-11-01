7af73ead4b0c349817ddab5af2f96424dbc5566a
Igor Motorin <igor.motorin@gmail.com>
2020-11-01 23:25:36 +0300

    feat: repeater recursive

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,47 @@
-const CustomError = require("../extensions/custom-error");
+let matr = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
 
+for (let i = 0; i < 25; i++) {
+  matr.push(matr[i].slice(1) + matr[i][0])
+}
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
+  }
+
+  encrypt(phrase, key) {
+    if (phrase === undefined || key === undefined) {
+      throw new Error('Error')
+    }
+    phrase = phrase.toUpperCase();
+    let res = '';
+    key = key.repeat(Math.ceil(phrase.length / key.length)).toUpperCase();
+    for (let i = 0; i < phrase.length; i++) {
+      const k = matr.find(str => str[0] === key[i]);
+      const j = matr.findIndex(str => str[0] === phrase[i]);
+      res += k[j] || phrase[i];
+      if (!k[j]) {
+        key = ' ' + key;
+      }
+    }
+    return this.direct ? res : [...res].reverse().join('');
+  }
+
+  decrypt(phrase, key) {
+    if (phrase === undefined || key === undefined) {
+      throw new Error('Error')
+    }
+    phrase = phrase.toUpperCase();
+    let res = '';
+    key = key.repeat(Math.ceil(phrase.length / key.length)).toUpperCase();
+    for (let i = 0; i < phrase.length; i++) {
+      const k = matr.find(str => str[0] === key[i]);
+      const j = k.indexOf(phrase[i]);
+      res += matr[0][j] || phrase[i];
+      if (!matr[0][j]) {
+        key = ' ' + key;
+      }
+    }
+    return this.direct ? res : [...res].reverse().join('');
   }
 }
 
